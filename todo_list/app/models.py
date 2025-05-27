1c8e14ab1b98f55f6faa98c34712e32ff5d63af3
fathss <alwifathu@gmail.com>
2025-05-27 13:36:33 +0700

    Added automatic deletion after task is done

--- todo_list/app/models.py
+++ todo_list/app/models.py
@@ -7,3 +7,4 @@ class Task(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     task: so.Mapped[str] = so.mapped_column(sa.String(100), nullable=False)
     completed: so.Mapped[Optional[bool]] = so.mapped_column(sa.Boolean, default=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
--- todo_list/app/routes.py
+++ todo_list/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
 from app.models import Task
+from datetime import datetime, timedelta
 
 @app.route('/')
 def index():
@@ -23,10 +24,24 @@ def update(id):
         return redirect(url_for('index'))
     return render_template('edit.html', id=id, task=task)
 
+@app.before_request
+def delete_old_completed_tasks():
+    threshold = datetime.utcnow() - timedelta(seconds=5)
+    old_tasks = Task.query.filter(Task.completed == True, Task.completed_at != None, Task.completed_at < threshold).all()
+    for task in old_tasks:
+        db.session.delete(task)
+    if old_tasks:
+        db.session.commit()
+
 @app.route('/toggle/<int:task_id>', methods=['POST'])
 def toggle_task(task_id):
     task = Task.query.get(task_id)
-    task.completed = not task.completed
+    if not task.completed:
+        task.completed = True
+        task.completed_at = datetime.utcnow()
+    else:
+        task.completed = False
+        task.completed_at = None
     db.session.commit()
     return redirect(url_for('index'))
 
